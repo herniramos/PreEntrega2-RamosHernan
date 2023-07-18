199c082233a75145f7f47f06c7750f60cb4f4138
Ramos Hernan <herni.ramoss@gmail.com>
2023-07-17 22:48:41 -0300

    commit

---
+++ src/components/CartWidgets/CartWidgets.js
@@ -0,0 +1,10 @@
+import "./CartWidgets.css"
+const CartWidgets = () => {
+  return (
+    <div className="Carrito">
+      <img alt="carrito" src="/img/carrito.png" width="25px" />0
+    </div>
+  );
+};
+
+export default CartWidgets;
---
+++ src/data/products.js
@@ -0,0 +1,104 @@
+export const products = [
+    {
+        id: '1',
+        precio : 7361,
+        title: 'Remera White',
+        titleDetail: 'Descripción del producto',
+        detail: 'Remera white LIFT',
+        image: "/img/white.jpg",
+        stock: 24,
+        category: 'remeras'
+    },
+    {
+        id: '2',
+        precio: 6893,
+        title: 'Wear LIFT',
+        titleDetail: 'Descripción del producto',
+        detail: 'Wear LIFT',
+        image: '/img/wear.jpg',
+        stock: 20,
+        category: 'remeras'        
+    },
+    {
+        id: '3',
+        precio: 4025,
+        title: 'Sleeve LIFT',
+        titleDetail: 'Descripción del producto',
+        detail: 'Sleeve LIFT',
+        image: '/img/sleeve.jpg',
+        stock: 18,
+        category: 'remeras'
+    },
+    {
+        id: '4',
+        precio: 6834,
+        title: 'Musculosa LIFT',
+        titleDetail: 'Descripción del producto',
+        detail: 'Musculosa LIFT',
+        image: '/img/musc.jpg',
+        stock: 20,
+        category: 'remeras'
+    },
+    {
+        id: '5',
+        precio: 5719,
+        title: 'Hoodie LIFT',
+        titleDetail: 'Descripción del producto',
+        detail: 'Hoodie LIFT',
+        image: '/img/hoodie.jpg',
+        stock: 25,
+        category: 'remeras'
+    },
+    {
+        id: '6',
+        precio: 9515,
+        title: 'Pantalones LIFT',
+        titleDetail: 'Descripción del producto',
+        detail: 'El mejor alimento para gato adulto sabor pescado.',
+        image: '/img/pants.jpg',
+        stock: 35,
+        category: 'pantalones'
+    },
+    {
+        id: '7',
+        precio: 3690,
+        title: 'Short',
+        titleDetail: 'Descripción del producto',
+        detail: 'Short LIFT',
+        image: '/img/short.jpg',
+        stock: 12,
+        category: 'pantalones'
+    }
+    ,
+    {
+        id: '8',
+        precio: 11770,
+        title: 'Malla LIFT',
+        titleDetail: 'Descripción del producto',
+        detail: 'Malla LIFT.',
+        image: '/img/malla.jpg',
+        stock: 14,
+        category: 'pantalones'
+    },
+    {
+        id: '9',
+        precio: 29995,
+        title: 'Mochila LIFT',
+        titleDetail: 'Descripción del producto',
+        detail: 'Mochila',
+        image: '/img/mochila.jpg',
+        stock: 9,
+        category: 'others'
+    }
+    ,
+    {
+        id: '10',
+        precio: 6299,
+        title: 'Gorro LIFT',
+        titleDetail: 'Descripción del producto',
+        detail: 'Gorro de lana.',
+        image: '/img/others.jpg',
+        stock: 16,
+        category: 'others'
+    }   
+];
